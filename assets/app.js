1efee24e81abbe83959153abf4b30854dd9ea1f4
christian <arthurchristian111@gmail.com>
2022-10-08 23:34:20 +0000

    manipulated the giveaway text using the DOM

--- assets/app.js
+++ assets/app.js
@@ -12,6 +12,7 @@ const months = [
     "November",
     "December",
 ];
+
 const weekdays = [
     "Sunday",
     "Monday",
@@ -21,3 +22,36 @@ const weekdays = [
     "Friday",
     "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway")
+const deadLine = document.querySelector(".deadLine")
+const item = document.querySelectorAll(".deadLine h4")
+
+const futureDate = new Date(2022, 11, 25, 12, 30, 00);
+console.log(futureDate)
+
+const year = futureDate.getFullYear();
+
+let month = futureDate.getMonth();
+month = months[month];
+
+let weekday = futureDate.getDay();
+weekday = weekdays[weekday]
+console.log(weekday)
+
+const hour = futureDate.getHours();
+const minute = futureDate.getMinutes();
+const date = futureDate.getDate();
+
+giveaway.innerHTML = `Giveaway Ends on ${weekday}, ${date} ${month} ${year} ${hour}:${minute}am`
+
+//future time
+const futureTime = futureDate.getTime();
+// console.log(futureTime)
+
+const getRemainingTime = () => {
+    const today = new Date().getTime();
+    // console.log(today)
+}
+
+getRemainingTime();
